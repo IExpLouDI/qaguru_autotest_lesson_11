879a1229952ab1e7e18e85ccc79e7f37ced5f2f1
Viacheslav Suchkov <iexploudi@gmail.com>
2025-06-24 20:14:39 +0400

    init project (refactoring)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,29 +4,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="session")
-def init_browser():
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {"enableVideo": False},
-    }
-    option = Options()
-    options = webdriver.ChromeOptions()
+@pytest.fixture(scope="function")
+def browser_():
+
+    options = Options()
     options.add_argument("--start-maximized")
-    # options.add_argument("--headless")
+    # options.add_argument("--headless")  # Uncomment for headless mode
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.page_load_strategy = "eager"
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
-	options.capabilities.update(selenoid_capabilities)
-    
+
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,  # Added VNC for visibility
+            "enableVideo": True,
+            "enableLog": True,  # Added logging
+        },
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    # Initialize remote driver
     driver = webdriver.Remote(
-        command_executor="https://selenoid.autotests.cloud/wd/hub",
-        desired_capabilities=selenoid_capabilities,
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options,  # Using options instead of desired_capabilities
     )
-    
-    browser.config.driver_options = options
-    browser = Browser(Config())
+
+    # Initialize Selene browser
+    browser = Browser(Config(driver))
+
     yield browser
-    browser.close()
+
+    # Teardown
+    browser.quit()  # Using quit() instead of close() for proper cleanup
--- tests/test_search_from_form.py
+++ tests/test_search_from_form.py
@@ -2,11 +2,11 @@ from model.pages.registration_page import RegistrationPage
 from test_data.users import UserInfo
 
 
-def test_registration_page(init_browser):
-	registration_page = RegistrationPage(init_browser)
-	user = UserInfo()
+def test_registration_page(browser_):
+    registration_page = RegistrationPage(browser_)
+    user = UserInfo()
 
-	registration_page.open_form()
-	registration_page.registration(user)
+    registration_page.open_form()
+    registration_page.registration(user)
 
-	registration_page.should_person_info(user.user_full_info())
+    registration_page.should_person_info(user.user_full_info())
